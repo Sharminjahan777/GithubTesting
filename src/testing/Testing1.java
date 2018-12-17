79cf0f2952411b2878b224d40a5c645d078e84e9
Sharminjahan777 <jahansharmin096@gmail.com>
2018-12-17 13:21:37 -0500

    new lines

--- src/testing/Testing1.java
+++ src/testing/Testing1.java
@@ -3,8 +3,11 @@ package testing;
 public class Testing1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		System.out.print("This a github testing method");
+		System.out.print("This a github testing method");
+		System.out.print("This a github testing method");
+		
 	}
 
 }
